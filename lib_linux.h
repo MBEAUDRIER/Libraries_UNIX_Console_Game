e189749d3210912d2eff873f929c0937010f3114
MBEAUDRIER <38099226+MBEAUDRIER@users.noreply.github.com>
2018-04-13 14:53:30 +0200

    test

--- lib_linux.h
+++ lib_linux.h
@@ -34,11 +34,20 @@ void init()
     tcsetattr(0, TCSANOW, &new_termios);
 }
 
-int get_key()
+char get_key()
 {
+    int val;
+    char c=0;
     struct timeval tv = { 0L, 0L };
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(0, &fds);
-    return select(1, &fds, NULL, NULL, &tv);
+    val=select(1, &fds, NULL, NULL, &tv);
+    
+    if (val !=0)
+    {
+        scanf("%c",&c);
+    }
+    return(c);
+    
 }
